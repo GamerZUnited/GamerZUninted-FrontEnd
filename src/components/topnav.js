29c0b743e539785f50203481f4402a6069b8fb85
JRob1991 <jprobinson9791@gmail.com>
2015-12-15 16:47:47 -0500

    Bug fixed

--- src/components/topnav.js
+++ src/components/topnav.js
@@ -1,20 +1,11 @@
 import React from 'react';
 
 import { AppBar } from "material-ui";
-import { IconButton } from "material-ui";
-import { FlatButton } from "material-ui";
-import { NavigationClose } from "material-ui";
-
-
 
 class TopNav extends React.Component {
   render() {
     return (
       <AppBar
-        style="color:"
-        title="Title"
-        iconClassNameRight="muidocs-icon-navigation-expand-more"
-      />
       className="appbar"
       title="GamerZUnited"
       iconClassNameRight="muidocs-icon-navigation-expand-more" />
