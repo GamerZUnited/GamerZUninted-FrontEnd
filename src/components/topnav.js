08b3ea7f95f15b5620b65e3391c347fa55d044d1
JRob1991 <jprobinson9791@gmail.com>
2015-12-15 16:44:05 -0500

    Getting everything responsive

--- src/components/topnav.js
+++ src/components/topnav.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 import { AppBar } from "material-ui";
+import { IconButton } from "material-ui";
+import { FlatButton } from "material-ui";
+import { NavigationClose } from "material-ui";
+
+
 
 class TopNav extends React.Component {
   render() {
     return (
       <AppBar
-      title="GamerZUnited"
-      iconClassNameRight="muidocs-icon-navigation-expand-more" />
+        style="color:"
+        title="Title"
+        iconClassNameRight="muidocs-icon-navigation-expand-more"
+      />
     )
   }
 }
